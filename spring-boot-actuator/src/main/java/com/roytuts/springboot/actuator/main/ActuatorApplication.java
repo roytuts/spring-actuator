bd10367dda24920222e0e47a86206591beedc810
Soumitra Roy Sarkar <projwebinfo@gmail.com>
2020-03-06 10:12:24 +0530

    Add files via upload

---
+++ spring-boot-actuator/src/main/java/com/roytuts/springboot/actuator/main/ActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.roytuts.springboot.actuator.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorApplication.class, args);
+	}
+
+}
