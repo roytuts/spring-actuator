4bbee755811afc92a887300608d046219b07bb4c
Soumitra <projwebinfo@gmail.com>
2023-10-05 23:20:54 +0530

    Update GreetingEndpoint.java

--- spring-boot-actuator-custom-endpoint/src/main/java/com/roytuts/springboot/actuator/endpoint/GreetingEndpoint.java
+++ spring-boot-actuator-custom-endpoint/src/main/java/com/roytuts/springboot/actuator/endpoint/GreetingEndpoint.java
@@ -7,17 +7,19 @@ import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.roytuts.springboot.actuator.vo.Greeting;
+import com.roytuts.springboot.actuator.endpoint.vo.Greeting;
 
 @Component
 @Endpoint(id = "hello-endpoint")
 public class GreetingEndpoint {
 
 	private static final String template = "Hello, %s!";
+	
 	private final AtomicLong counter = new AtomicLong();
 
 	@ReadOperation
 	public Greeting sayHello(@RequestParam(defaultValue = "Stranger") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
+
 }
